db6e91afea9de2b7ca480e4f25c2206ade1bea40
ganghee <idiosauri@naver.com>
2019-11-29 22:11:53 +0900

    updated

---
+++ Seminar_5th/multer.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+aws.config.loadFromPath(__dirname + '/awsconfig.json');
+const s3 = new aws.S3();
+const upload = multer({ storage: multerS3({
+    s3: s3,
+    bucket: 's3sopt25',
+    acl: 'public-read',
+    key: function(req, file, cb) {
+        cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+        }
+    }) 
+});
+module.exports = upload;
